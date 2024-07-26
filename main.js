d6257751e32dba33927d3732ad35b9ab3d4b60d6
劉琨旴 <kx108028@gmail.com>
2024-07-26 17:28:50 +0800

    將所有html、css、js建立還原點

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log('Hello World!');
+document.getElementById('refreshButton').addEventListener('click', function() {
+ location.reload();
+});
+//聯絡我 > 表單
+document.addEventListener('DOMContentLoaded', function() {
+ var form = document.querySelector('form');
+ form.addEventListener('submit', function(event) {
+  // 防止預設表單提交
+  event.preventDefault();
+  // 從表單資料建立電子郵件正文
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+  var subject = name + '寄來的訊息';
+  var body = '姓名：' + name + '\n';
+  body += '信箱： ' + email + '\n\n';
+  body += '訊息：\n' + message;
+  // 建立 mailto URL
+  var mailtoUrl = 'mailto:kx108028@gmail.com' +
+   '?subject=' + encodeURIComponent(subject) +
+   '&body=' + encodeURIComponent(body);
+  // 開啟郵件客戶端
+  window.location.href = mailtoUrl;
+ });
+});
+(adsbygoogle = window.adsbygoogle || []).push({});
