043fa175f691551d3856d93b5e182cccc33f106a
劉琨旴 <kx108028@gmail.com>
2024-07-31 14:49:46 +0800

    修復了「導入網頁」和「索引網頁」的導入功能，修復錯誤的網址，加入未啟用的按鈕（為以後的功能預留空位）。

--- main.js
+++ main.js
@@ -21,9 +21,11 @@ document.addEventListener('DOMContentLoaded', function() {
    '?subject=' + encodeURIComponent(subject) +
    '&body=' + encodeURIComponent(body);
   // 開啟郵件客戶端
+
+//導入
   window.location.href = mailtoUrl;
  });
 });
-function redirectToImport(url) {
+function url(url) {
     window.location.href = url;
 }
